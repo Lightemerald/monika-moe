d3c08ea927790f2a8a356b16dbaa08746e235193
Lightemerald <light_emerald@aostia.com>
2022-12-19 17:05:34 +0100

    Update and fixes

--- api/server.js
+++ api/server.js
@@ -1,142 +1,111 @@
-const cors = require('cors');
-const mysql = require('mysql2');
-const express = require('express');
-const { apiPort, cdnPort, token, base } = require('../config/settings.json');
-const database = require('../config/database.json');
+const cors = require('cors')
+const mysql = require('mysql2')
+const express = require('express')
+const { apiPort, cdnPort, token, base } = require('../config/settings.json')
+const database = require('../config/database.json')
 
-
-const pool = mysql.createPool(database);
-const app = express();
-app.use(express.json());
+const pool = mysql.createPool(database)
+const app = express()
+app.use('/', express.static('../public'))
+app.use(express.json())
 app.use(cors({
-    origin: '*',
-}));
-
+  origin: '*'
+}))
 
 app.get(base, (req, res) => {
-
-    const authorization = req.header('authorization');
-    if (authorization === token) {
-        res.status(200).json({ code: 200, endpoints: ['get/', 'add/', 'delete/', 'update/', 'test/'] });
-    }
-    else {
-        res.status(401).json({ code: 401, message: 'Unauthorized' });
-    }
-
-});
-
+  const authorization = req.header('authorization')
+  if (authorization === token) {
+    res.status(200).json({ code: 200, endpoints: ['get/', 'add/', 'delete/', 'update/', 'test/'] })
+  } else {
+    res.status(401).json({ code: 401, message: 'Unauthorized' })
+  }
+})
 
 app.get(base + '/get', (req, res) => {
-
-    pool.query('SELECT * FROM images ORDER BY RAND() LIMIT 1', function(err, rows) {
-        if (rows.length >= 1 && !err) {
-            rows[0].image = `http://localhost:` + cdnPort + '/images/' + rows[0].filename;
-            rows[0].code = 200;
-            res.status(200).json(rows[0]);
-        }
-        else {
-            res.status(404).json({ code: 404, message: 'Image not found' });
-        }
-    });
-
-});
-
+  pool.query('SELECT * FROM images ORDER BY RAND() LIMIT 1', function (err, rows) {
+    if (rows.length >= 1 && !err) {
+      rows[0].image = 'http://localhost:' + cdnPort + '/images/' + rows[0].filename
+      rows[0].code = 200
+      res.status(200).json(rows[0])
+    } else {
+      res.status(404).json({ code: 404, message: 'Image not found' })
+    }
+  })
+})
 
 app.post(base + '/add', (req, res) => {
-
-    const data = req.body;
-    const authorization = req.header('authorization');
-    if (authorization === token) {
-        if (data.status !== 'undefined' && data.filename) {
-            pool.query(`INSERT INTO images(filename, nsfw) VALUES ("${data.filename.replace(/'/g, '\'')}", ${data.status})`, function(err) {
-                if (!err) {
-                    res.status(200).json({ code: 200, message: 'Image added!' });
-                }
-                else {
-                    res.status(200).json({ code: 500, message: 'Failed to add image!', err: err });
-                }
-            });
+  const data = req.body
+  const authorization = req.header('authorization')
+  if (authorization === token) {
+    if (data.status !== 'undefined' && data.filename) {
+      pool.query(`INSERT INTO images(filename, rating) VALUES ("${data.filename.replace(/'/g, '\'')}", ${data.status})`, function (err) {
+        if (!err) {
+          res.status(200).json({ code: 200, message: 'Image added!' })
+        } else {
+          res.status(200).json({ code: 500, message: 'Failed to add image!', err })
         }
-        else {
-            res.status(400).json({ code: 400, message: 'Missing argument' });
-        }
+      })
+    } else {
+      res.status(400).json({ code: 400, message: 'Missing argument' })
     }
-    else {
-        res.status(401).json({ code: 401, message: 'Unauthorized' });
-    }
-
-});
-
+  } else {
+    res.status(401).json({ code: 401, message: 'Unauthorized' })
+  }
+})
 
 app.post(base + '/delete', (req, res) => {
-
-    const data = req.body;
-    const authorization = req.header('authorization');
-    if (authorization === token) {
-        if (data.id) {
-            pool.query(`DELETE FROM images WHERE id=${data.id}`, function(err) {
-                if (!err) {
-                    res.status(200).json({ code: 200, message: 'Image deleted!' });
-                }
-                else {
-                    res.status(500).json({ code: 500, message: 'Failed to delete image!', err: err });
-                }
-            });
+  const data = req.body
+  const authorization = req.header('authorization')
+  if (authorization === token) {
+    if (data.id) {
+      pool.query(`DELETE FROM images WHERE id=${data.id}`, function (err) {
+        if (!err) {
+          res.status(200).json({ code: 200, message: 'Image deleted!' })
+        } else {
+          res.status(500).json({ code: 500, message: 'Failed to delete image!', err })
         }
-        else {
-            res.json({ code: 400, message: 'Missing argument' });
-        }
+      })
+    } else {
+      res.json({ code: 400, message: 'Missing argument' })
     }
-    else {
-        res.json({ code: 401, message: 'Unauthorized' });
-    }
-
-});
-
+  } else {
+    res.json({ code: 401, message: 'Unauthorized' })
+  }
+})
 
 app.post(base + '/update', (req, res) => {
-
-    const data = req.body;
-    const authorization = req.header('authorization');
-    if (authorization === token) {
-        if (data.id && data.filename) {
-            let query = `UPDATE images SET filename=\"${data.filename.replace(/'/g, '\'')}\" WHERE id=${data.id}`;
-            if (data.status !== 'undefined') {
-                query = `UPDATE images SET filename=\"${data.filename.replace(/'/g, '\'')}\" AND nsfw=${data.status} WHERE id=${data.id}`;
-            }
-            pool.query(query, function(err) {
-                if (err) {
-                    res.status(500).json({ code: 500, message: 'Failed to update AD!', err: err });
-                }
-                else {res.status(200).json({ code: 200, message: 'AD updated' });}
-            });
-        }
-        else {
-            res.status(400).json({ code: 400, message: 'Missing argument' });
-        }
+  const data = req.body
+  const authorization = req.header('authorization')
+  if (authorization === token) {
+    if (data.id && data.filename) {
+      let query = `UPDATE images SET filename="${data.filename.replace(/'/g, '\'')}" WHERE id=${data.id}`
+      if (data.status !== 'undefined') {
+        query = `UPDATE images SET filename="${data.filename.replace(/'/g, '\'')}" AND rating=${data.status} WHERE id=${data.id}`
+      }
+      pool.query(query, function (err) {
+        if (err) {
+          res.status(500).json({ code: 500, message: 'Failed to update AD!', err })
+        } else { res.status(200).json({ code: 200, message: 'AD updated' }) }
+      })
+    } else {
+      res.status(400).json({ code: 400, message: 'Missing argument' })
     }
-    else {
-        res.status(401).json({ code: 401, message: 'Unauthorized' });
-    }
-
-});
-
+  } else {
+    res.status(401).json({ code: 401, message: 'Unauthorized' })
+  }
+})
 
 // TEST endpoints
 app.get(base + '/test', (req, res) => {
-
-    res.status(200).json({ code: 200 });
-
-});
+  res.status(200).json({ code: 200 })
+})
 
 app.post(base + '/test', (req, res) => {
-
-    const data = req.body;
-    res.status(200).json({ code: 200, data });
-
-});
+  const data = req.body
+  res.status(200).json({ code: 200, data })
+})
 
 // run the API
 app.listen(apiPort, () => {
-    console.log(`running at port ${apiPort}`);
-});
+  console.log(`running at port ${apiPort}`)
+})
---
+++ public/scripts/script.js
@@ -0,0 +1,130 @@
+/* eslint-disable no-unused-vars */
+const { api } = require('./config.json')
+
+// Change style of navbar on scroll
+window.onscroll = function () { myFunction() }
+function myFunction () {
+  const navbar = document.getElementById('myNavbar')
+  if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+    navbar.className = 'w3-bar' + ' w3-card' + ' w3-animate-top' + ' w3-white'
+  } else {
+    navbar.className = navbar.className.replace(' w3-card w3-animate-top w3-white', '')
+  }
+}
+
+// Used to toggle the menu on small screens when clicking on the menu button
+function toggleFunction () {
+  const navbar = document.getElementById('myNavbar')
+  const x = document.getElementById('navDemo')
+  if (x.className.indexOf('w3-show') === -1) {
+    x.className += ' w3-show'
+    navbar.className += ' w3-black'
+  } else {
+    x.className = x.className.replace(' w3-show', '')
+    navbar.className = navbar.className.replace(' w3-black', '')
+  }
+}
+
+function toggleAppMenu () {
+  const navbar = document.getElementById('myNavbar')
+  const x = document.getElementById('appMenu')
+  if (x.className.indexOf('w3-show') === -1) {
+    x.className = x.className.replace(' w3-hide', ' w3-show')
+    navbar.className += ' w3-black'
+  } else {
+    x.className = x.className.replace(' w3-show', ' w3-hide')
+    navbar.className = navbar.className.replace(' w3-black', '')
+  }
+}
+
+function toggleSFW () {
+  const sfw = document.getElementById('sfwbtn')
+  const value = document.getElementById('sfwbtn').value
+  if (value === '1') {
+    document.getElementById('sfwbtn').value = 0
+    document.getElementById('sfwbtn').textContent = 'SFW'
+    sfw.className = sfw.className.replace(' w3-border-red w3-text-red w3-hover-red', ' w3-border-green w3-text-green w3-hover-green')
+  } else {
+    document.getElementById('sfwbtn').value = 1
+    document.getElementById('sfwbtn').textContent = 'NSFW'
+    sfw.className = sfw.className.replace(' w3-border-green w3-text-green w3-hover-green', ' w3-border-red w3-text-red w3-hover-red')
+  }
+}
+
+function toggleSRC () {
+  if (document.getElementById('srcbtn').value === 'api') {
+    document.getElementById('srcbtn').value = 'reddit'
+    document.getElementById('srcbtn').textContent = 'REDDIT'
+    document.getElementById('srcbtn').className = document.getElementById('srcbtn').className.replace(' w3-border-green w3-text-green w3-hover-green', ' w3-border-orange w3-text-orange w3-hover-orange')
+  } else {
+    document.getElementById('srcbtn').value = 'api'
+    document.getElementById('srcbtn').textContent = 'API'
+    document.getElementById('srcbtn').className = document.getElementById('srcbtn').className.replace(' w3-border-orange w3-text-orange w3-hover-orange', ' w3-border-green w3-text-green w3-hover-green')
+  }
+}
+
+function closeMenu () {
+  const navbar = document.getElementById('myNavbar')
+  const menu = document.getElementById('appMenu')
+  menu.className = menu.className.replace(' w3-show', ' w3-hide')
+  navbar.className = navbar.className.replace(' w3-black', '')
+}
+
+function fetchMonika () {
+  closeMenu()
+  document.getElementById('image').style.opacity = 0.2
+  document.getElementById('image').src = './img/loading.gif'
+  if (document.getElementById('srcbtn').value === 'api') {
+    fetch(api, { cache: 'no-cache', mode: 'cors' })
+      .then(res => res.json())
+      .then(json => update(json))
+  } else {
+    const url = 'https://www.reddit.com/r/Monikafandom/random/.json?f=flair_name%3A%22Found+art%22'
+    fetch(url, { cache: 'no-cache' })
+      .then(res => res.json())
+      .then(json => updateReddit(json[0].data))
+  }
+}
+
+function updateReddit (data) {
+  const url = data.children[0].data.url
+
+  if (!url.includes('https://i.redd.it/')) {
+    fetchMonika()
+  } else {
+    const isnsfw = data.children[0].data.over_18
+    if (isnsfw && document.getElementById('sfwbtn').value === 0) {
+      fetchMonika()
+    } else {
+      document.getElementById('image').src = url
+      document.getElementById('image').style.opacity = 1
+    }
+  }
+}
+
+function update (json) {
+  console.log(json)
+  document.getElementById('image').src = json.image
+  document.getElementById('image').style.opacity = 1
+}
+
+function disableF5 (e) {
+  if ((e.which || e.keyCode) === 116) { e.preventDefault() }
+}
+
+addEventListener('keydown', disableF5)
+
+window.onload = function () {
+  if (localStorage.justMonika !== 1) {
+    dbox('Hi! This is your first time here,\nto get a new image click on the current one.')
+    localStorage.justMonika = 1
+  }
+  fetchMonika()
+}
+
+function dbox (msg) {
+  if (msg !== undefined) {
+    document.getElementById('boxTxt').innerHTML = msg
+    document.getElementById('boxBack').classList.add('show')
+  } else { document.getElementById('boxBack').classList.remove('show') }
+}
